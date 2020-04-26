a8df47547a9e262e1a7e328c3f3ab9106a39800b
maXchov <62498714+maXchov@users.noreply.github.com>
2020-04-26 09:37:35 +0300

    Add files via upload

---
+++ Bomb.shop.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+    <header>
+        
+       
+        <a href="#" id="el">Главное</a>
+        <a href="#" id="el">Покупка</a>
+        <a href="#" id="el">Контакты</a>
+        <a href="#" id="el">Профиль</a>
+        
+        
+    </header>
+    <body>
+        
+            <h2><a href="Profile.php" id="Profile">Профиль</a></h2>
+
+            
+            
+
+           
+            
+        
+
+            
+
+
+
+    </body>
+    <footer>    
+
+        <a href="https://vk.com/id531315835?utm_source=vk&utm_medium=cpc&utm_campaign={campaign_id}&utm_content={ad_id}"  ></a>
+
+        <a href="otsivi.php" id="otz">oтзывы</a>
+
+    </footer>
+</html>
---
+++ Index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        html,body{display: flex;
+            flex-direction: column;
+            text-align: center;
+            justify-content: space-around;
+            height: 100%;
+            background-image:url(image/Bombshop.png) ; 
+            background-blend-mode: multiply;
+            background-color: rgba(180, 180, 177, 0.5);
+            background-repeat: no-repeat;
+            background-size: cover;
+            background-position: center;
+            color:  #ffc800;
+        }
+        
+    </style>
+</head>
+<body>
+    <div id="heddr">
+        <a href="login.php">Войти</a>
+        <a href="reg.php">Регистрация</a>
+    </div>
+</body>
+</html>
