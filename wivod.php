31accb15787869e382fedb15989ab204dd902192
maXchov <62498714+maXchov@users.noreply.github.com>
2020-05-31 11:50:42 +0300

    Add files via upload

--- wivod.php
+++ wivod.php
@@ -1,5 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        .container{
+	display: flex;
+	text-align: center;
+    position: fixed;
+	}
+    </style>
+</head>
+<body>
+   <h1 class="container"><a href="index.html">Спасибо за отзыв!</a> </h1> 
+</body>
+</html>
 <? 
-session_start();
+
 
 
 
